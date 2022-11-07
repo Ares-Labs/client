b49754bb0c91af7778b141c8e1b60d1807cff18e
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-11-07 14:47:20 +0100

    added comment and simplified definitions

--- src/utils/events.js
+++ src/utils/events.js
@@ -4,55 +4,56 @@ const CHNL_TO_SERVER = "events.to.martians";
 const EVENTBUS_PATH = "http://localhost:8080/events";
 
 class Gateway {
-  #eb;
-  #subscribers;
+    #eb;
+    #subscribers;
 
-  constructor() {
-    this.#eb = new EventBus(EVENTBUS_PATH);
-    this.#subscribers = new Object();
+    constructor() {
+        this.#eb = new EventBus(EVENTBUS_PATH);
+        this.#subscribers = {};
 
-    this.#eb.onopen = () => {
-      // TODO: Bind to individual event for user.
-      this.#registerHandler(CHNL_TO_SERVER, this.#onMessage);
-    };
-  }
-
-  #registerHandler(chanel, callback) {
-    const bound_callback = callback.bind(this);
-    this.#eb.registerHandler(chanel, bound_callback);
-  }
-
-  #onMessage(error, message) {
-    if (error) {
-      // TODO: return error
-      return;
+        this.#eb.onopen = () => {
+            // TODO: Bind to individual event for user.
+            this.#registerHandler(CHNL_TO_SERVER, this.#onMessage);
+        };
     }
 
-    const parsed = JSON.parse(message);
-
-    if (parsed.event === undefined || parsed.data === undefined) {
-      // Invalid event
-      return;
+    #registerHandler(chanel, callback) {
+        const bound_callback = callback.bind(this);
+        this.#eb.registerHandler(chanel, bound_callback);
     }
 
-    this.#invokeSubscriptions("all", parsed.data);
-    this.#invokeSubscriptions(parsed.event, parsed.data);
-  }
+    #onMessage(error, message) {
+        if (error) {
+            // TODO: return error
+            return;
+        }
 
-  #invokeSubscriptions(event, data) {
-    this.#subscribers[event].forEach((callback) => callback(data));
-  }
+        const parsed = JSON.parse(message);
 
-  subscribe(event, callback) {
-    const event_subscribers = this.#subscribers[event];
+        if (parsed.event === undefined || parsed.data === undefined) {
+            // Invalid event
+            return;
+        }
 
-    if (event_subscribers === undefined) {
-      this.#subscribers[event] = new Array();
-      return this.subscribe(event, callback);
+        this.#invokeSubscriptions("all", parsed.data);
+        this.#invokeSubscriptions(parsed.event, parsed.data);
     }
 
-    event_subscribers.push(callback);
-  }
+    #invokeSubscriptions(event, data) {
+        this.#subscribers[event].forEach((callback) => callback(data));
+    }
+
+    subscribe(event, callback) {
+        const event_subscribers = this.#subscribers[event];
+
+        // Create new event if it doesn't exist
+        if (event_subscribers === undefined) {
+            this.#subscribers[event] = [];
+            return this.subscribe(event, callback);
+        }
+
+        event_subscribers.push(callback);
+    }
 }
 
 export default new Gateway();

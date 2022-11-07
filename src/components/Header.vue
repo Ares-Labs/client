92c2b2986140e179b7fe00212ee2952dc40fef3d
jurgen.telegrafi <jurgen.telegrafi@student.howest.be>
2022-11-07 16:08:09 +0100

    main header as a vue component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img src="#" alt="ares-lab-icon">
+    <p>Manage property</p>
+    <p>Live data</p>
+    <p>Map</p>
+    <p>Recent activity</p>
+    <p>Pricing / Upgrade</p>
+    <img src="#" alt="profile icon">
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 3rem;
+  margin-bottom: 20rem;
+  background-color: orange;
+  border-radius: 1rem;
+  box-shadow: black 0.1rem 0.1rem 1.5rem 0.3rem;
+}
+
+</style>

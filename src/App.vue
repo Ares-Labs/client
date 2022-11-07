bbe82183ed6684c9ff04d167053c59af5f15c8f9
Arthurdw <dev@arthurdw.com>
2022-11-07 16:00:05 +0100

    :sparkles: Added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,3 @@
-<script setup>
-import Gateway from "./utils/events.js";
-
-// Example gateway subscription
-Gateway.subscribe("event", (data) => {
-  console.log(data);
-});
-</script>
-
 <template>
-  <header>
-    <h1>Ares LABS</h1>
-  </header>
-
-  <main>
-    <p>Hello</p>
-  </main>
+  <router-view></router-view>
 </template>
-
-<style lang="scss" scoped>
-main {
-  p {
-    color: red;
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import "./assets/css/reset.css";
 
+import { createRouter, createWebHistory } from "vue-router";
+
 import App from "./App.vue";
+import DashboardPage from "./pages/DashboardPage.vue";
+import LiveDataPage from "./pages/LiveDataPage.vue";
+import ManagePropertyPage from "./pages/ManagePropertyPage.vue";
 import { createApp } from "vue";
 
-createApp(App).mount("#app");
+const routes = [
+  { path: "/", component: DashboardPage },
+  { path: "/live-data", component: LiveDataPage },
+  { path: "/manage-property", component: ManagePropertyPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");

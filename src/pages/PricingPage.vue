71ee503189b0a4e5e1827a28a73b5f99346c2b7d
jurgen.telegrafi <jurgen.telegrafi@student.howest.be>
2022-11-09 17:46:30 +0100

    pricing page base html & css done - still some refinement needed

--- src/pages/PricingPage.vue
+++ src/pages/PricingPage.vue
@@ -1,54 +1,52 @@
 <script>
 import Header from "../components/Header.vue";
-import CheckmarksOrNo from "../components/CheckmarksOrNo.vue";
 
 export default {
-  name: "PricingPage",
   components: {
     Header,
-    CheckmarksOrNo,
   }
-
 };
+
 </script>
 
 <template>
-  <header/>
+  <Header/>
   <main>
     <h1>Compare Plan Features</h1>
-    <p>Pick a plan that suits your needs.</p>
-
+    <h4>Pick a plan that suits your needs.</h4>
     <article>
       <div id="select-plan-wrapper">
         <h2>Select Plan</h2>
         <div>
-          <p>MONTHLY</p>
+          <p class="selected">MONTHLY</p>
           <p>ANNUAL</p>
         </div>
-        <p>Cameras</p>
-        <p>Live camera footage</p>
-        <p>Automatic locking door</p>
-        <p>Drone security</p>
-        <p>Graph logging</p>
+        <div>
+          <p>Cameras</p>
+          <p>Live camera footage</p>
+          <p>Automatic locking door</p>
+          <p>Drone security</p>
+          <p>Graph logging</p>
+        </div>
       </div>
       <div id="basic-plan-wrapper">
         <p>current</p>
         <div>
           <h3>Basic</h3>
           <div>
-            <img src="#" alt="mars-coin">
+            <img src="../images/mars-coin.png" alt="mars-coin">
             <p>59</p>
           </div>
           <p>per month</p>
         </div>
-          <div class="checked-or-not">
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-          </div>
-          <button>SELECT PLAN</button>
+        <div class="checked-or-not">
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/x.png" alt="x"/>
+          <img src="../images/x.png" alt="x"/>
+          <img src="../images/x.png" alt="x"/>
+        </div>
+        <button>SELECT PLAN</button>
 
       </div>
       <div id="premium-plan-wrapper">
@@ -57,38 +55,38 @@ export default {
         <div>
           <h3>Premium</h3>
           <div>
-            <img src="#" alt="mars-coin">
-            <p>59</p>
+            <img src="../images/mars-coin.png" alt="mars-coin">
+            <p>79</p>
           </div>
           <p>per month</p>
         </div>
-          <div class="checked-or-not">
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-          </div>
-          <button>SELECT PLAN</button>
+        <div class="checked-or-not">
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/x.png" alt="x"/>
+          <img src="../images/x.png" alt="x"/>
+        </div>
+        <button>SELECT PLAN</button>
       </div>
       <div id="optimum-plan-wrapper">
         <p>current</p>
         <div>
           <h3>Optimum</h3>
           <div>
-            <img src="#" alt="mars-coin">
-            <p>59</p>
+            <img src="../images/mars-coin.png" alt="mars-coin">
+            <p>99</p>
           </div>
           <p>per month</p>
         </div>
-          <div class="checked-or-not">
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-            <CheckmarksOrNo/>
-          </div>
-          <button>SELECT PLAN</button>
+        <div class="checked-or-not">
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+          <img src="../images/checkmark.png" alt="checkmark"/>
+        </div>
+        <button>SELECT PLAN</button>
 
       </div>
     </article>
@@ -100,50 +98,111 @@ export default {
   display: none;
 }
 
+h1 {
+  display: flex;
+  justify-content: center;
+  font-size: 2rem;
+  margin-top: 5rem;
+}
+
+h4 {
+  display: flex;
+  justify-content: center;
+  font-size: 1rem;
+  margin-top: 2rem;
+  opacity: 50%;
+  margin-bottom: 4rem;
+}
+
 article {
   display: flex;
-  margin-top: 10rem;
-  margin-left: 10rem;
+  justify-content: center;
+
 }
 
-.checked-or-not {
-  display: flex;
-  flex-direction: column;
+#optimum-plan-wrapper {
+  border-radius: 0 0.3rem 0.3rem 0;
 }
 
-#basic-plan-wrapper,#premium-plan-wrapper ,#optimum-plan-wrapper {
+#basic-plan-wrapper, #premium-plan-wrapper, #optimum-plan-wrapper {
+  border: solid black;
+  border-left: none;
+
   div:first-of-type {
     background-color: #e8e8e8;
     text-align: center;
-    padding: 1rem 2rem;
+    padding: 1rem 6rem;
+
+    div {
+      display: flex;
+      font-size: 2rem;
+      font-weight: bold;
+
+      img {
+        width: 2rem;
+        margin-right: 0.2rem;
+      }
+    }
+
+    p:nth-child(3) {
+      opacity: 40%;
+    }
   }
-  border: solid black;
-  border-radius: 0.5rem;
+}
+
+.selected {
+  background-color: black;
+  color: white;
 }
 
 #select-plan-wrapper {
   border: solid black;
-  padding: 3rem 1rem 2rem 1rem;
+  border-radius: 0.3rem 0 0 0.3rem;
+  padding: 3rem 3rem 2rem 3rem;
+
   h2 {
     margin-bottom: 1rem;
   }
-  div {
+
+  div:first-of-type {
     display: flex;
-    margin-bottom: 3rem;
-    p:last-of-type{
-      margin-left: 1rem;
+    margin-bottom: 3.75rem;
+    border: solid black 0.2rem;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+
+    p:last-of-type {
+      margin-left: 0.5rem;
+      padding-left: 0.5rem;
+      border-left: solid black;
     }
   }
-  p {
-    margin-bottom: 2.5rem;
-    border-bottom: solid black;
-  }
 
+  div:last-of-type {
+    p {
+      margin-bottom: 3rem;
+      border-bottom: solid black 0.1rem;
+      padding-bottom: 0.5rem;
+    }
+  }
 }
 
-svg {
-  width: 2rem;
-  margin: 1rem auto;
-  border-bottom: solid black;
+
+.checked-or-not {
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 2rem;
+    margin: 1rem auto;
+    border-bottom: solid black 0.1rem;
+    padding-bottom: 0.5rem;
+  }
+}
+
+button {
+  display: flex;
+  margin: 1rem auto 1rem auto;
+  text-align: center;
 }
 </style>

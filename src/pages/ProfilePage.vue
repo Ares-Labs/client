ea7357efa0cd3c863f1efa7c5238aee738fd4fc2
Firxstartxr <ryazanovprokhor@gmail.com>
2022-11-10 15:16:33 +0100

    compiled scss to css due to browser warnings

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeaderWithoutLinks from "@/components/Header/HeaderWithoutLinks.vue";
+</script>
+
+<template>
+  <HeaderWithoutLinks></HeaderWithoutLinks>
+</template>
+
+<style scoped>
+
+</style>

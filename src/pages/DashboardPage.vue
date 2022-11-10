405235c1648e446b19eb16739c6007ffbd30e0aa
Firxstartxr <ryazanovprokhor@gmail.com>
2022-11-10 16:44:52 +0100

    changed naming

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -9,7 +9,7 @@ import HeaderWithoutLinks from "../components/Header/HeaderWithoutLinks.vue";
         <div>
           <router-link to="/choose-property"><p>Manage Property</p></router-link>
           <router-link to="/live-data"><p>Live Data</p></router-link>
-          <router-link to="/"><p>Recent activity</p></router-link>
+          <router-link to="/"><p>Statistics</p></router-link>
         </div>
         <div>
           <router-link to="/"><p>Map</p></router-link>

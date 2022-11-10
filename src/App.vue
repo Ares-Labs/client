b77d8978934a3ee9cb81190c453fb2367a0678b0
Arthurdw <dev@arthurdw.com>
2022-11-10 18:45:38 +0100

    :sparkles: Added an example of the gateway usage

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,96 @@ if (!Gateway.isInitialized) {
 
   Gateway.init(id);
 }
+
+const exampleUsage = async () => {
+  await Gateway.execute(Gateway.queries.ADD_PROPERTY, {
+    clientId: Gateway.clientId,
+    location: "test",
+    tier: 1,
+    x: 50,
+    y: 100,
+    description: "test",
+  });
+
+  const { pendingProperties } = await Gateway.execute(
+    Gateway.queries.GET_PENDING_PROPERTIES
+  );
+
+  const prop = pendingProperties[pendingProperties.length - 1];
+
+  await Gateway.execute(Gateway.queries.CHANGE_PROPERTY_STATUS, {
+    propertyId: prop.id,
+    status: "APPROVED",
+  });
+
+  await Gateway.execute(Gateway.queries.CHANGE_PROPERTY_SIZE, {
+    propertyId: prop.id,
+    width: 100,
+    height: 100,
+  });
+
+  await Gateway.execute(Gateway.queries.ADD_ALERTS, {
+    propertyId: prop.id,
+    userId: "bedb80fc-55f3-11ed-b681-07688aa63f8a",
+  });
+
+  for (var i = 1; i < 4; i++) {
+    const { id: cameraId } = await Gateway.execute(
+      Gateway.queries.ADD_EQUIPMENT_PROPERTY,
+      {
+        propertyId: prop.id,
+        equipmentType: 1,
+        description: "test",
+      }
+    );
+
+    for (var j = 0; j < 3; j++) {
+      await Gateway.execute(Gateway.queries.ADD_VISITOR, {
+        propertyId: prop.id,
+        userId: "bedb80fc-55f3-11ed-b681-07688aa63f8a",
+        cameraId,
+      });
+    }
+  }
+
+  const visitors = await Gateway.execute(Gateway.queries.GET_WEEKLY_VISITORS, {
+    propertyId: prop.id,
+  });
+  console.log(visitors);
+
+  const { equipment } = await Gateway.execute(
+    Gateway.queries.GET_EQUIPMENT_PROPERTY,
+    {
+      propertyId: prop.id,
+    }
+  );
+  console.log(equipment);
+
+  await Gateway.execute(Gateway.queries.REMOVE_EQUIPMENT_PROPERTY, {
+    propertyId: prop.id,
+    equipmentId: equipment[0].id,
+  });
+
+  await Gateway.execute(Gateway.queries.GET_EQUIPMENT_PROPERTY, {
+    propertyId: prop.id,
+  });
+
+  const { equipmentTypes } = await Gateway.execute(
+    Gateway.queries.GET_EQUIPMENT_TYPES
+  );
+  console.log(equipmentTypes);
+
+  const user = await Gateway.execute(Gateway.queries.GET_USER, {
+    userId: Gateway.clientId,
+  });
+  console.log(user);
+};
+
+Gateway.onReady(exampleUsage);
+
+Gateway.subscribe(Gateway.events.ALERTS, (data) => {
+  console.log("Received alert:", data);
+});
 </script>
 
 <template>

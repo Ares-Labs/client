830ed73baa31f41ae02339fe177d2a01e7b6b0b5
jurgen.telegrafi <jurgen.telegrafi@student.howest.be>
2022-12-08 13:32:50 +0100

    refactored to be able to add multiple graphs

--- src/components/Graphs/BarChartGraph.vue
+++ src/components/Graphs/BarChartGraph.vue
@@ -1,44 +1,39 @@
 <script>
+import {Bar} from 'vue-chartjs';
 import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js';
-import { Bar } from "vue-chartjs";
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
-  name: 'BarChart',
+  name: 'BarChartGraph',
   components: { Bar },
   computed: {
-    styles() {
+    myStyles() {
       return {
-        backgroundColor: 'white',
-        padding: '2rem',
-        borderRadius: '1rem',
-        boxShadow: '0 0 1rem 0.1rem black',
-        width: '60rem',
-        height: '30rem',
+        height: '50vh',
+        width: '25vw',
       };
-    },
+    }
   },
   data() {
     return {
       chartData: {
         labels: ['January', 'February', 'March'],
-        datasets: [{data: [40, 20, 12]}]
+        datasets: [{label: "Monthly visitors", data: [40, 20, 12]}]
       },
       chartOptions: {
-        responsive: false,
+        responsive: false
       }
     };
   }
 };
 </script>
-<script setup> </script>
 <template>
   <Bar
       id="my-chart-id"
+      :style="myStyles"
       :options="chartOptions"
       :data="chartData"
-      :style="styles"
   />
 </template>
 
--- src/pages/StatisticsPage.vue
+++ src/pages/StatisticsPage.vue
@@ -1,64 +1,30 @@
-<script>
-import {Bar} from 'vue-chartjs';
-import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js';
+<script setup>
 import Header from "../components/Header/Header.vue";
-import Graphs from "../components/BarChart.vue";
+import BarChartGraph from "../components/Graphs/BarChartGraph.vue";
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
-
-export default {
-  name: 'BarChart',
-  components: {Graphs, Header, Bar},
-  computed: {
-    styles() {
-      return {
-        backgroundColor: 'white',
-        padding: '2rem',
-        borderRadius: '1rem',
-        boxShadow: '0 0 1rem 0.1rem black',
-        width: '60rem',
-        height: '30rem',
-      };
-    },
-  },
-  data() {
-    return {
-      chartData: {
-        labels: ['January', 'February', 'March'],
-        datasets: [{data: [40, 20, 12]}]
-      },
-      chartOptions: {
-        responsive: false,
-      }
-    };
-  }
-};
 </script>
 
 <template>
   <Header/>
-  <div class="graphs"></div>
-  <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-      :style="styles"
-  />
+  <div class="graphs">
+    <BarChartGraph/>
+    <BarChartGraph/>
+    <BarChartGraph/>
+    <BarChartGraph/>
+    <BarChartGraph/>
+  </div>
+
 
 </template>
 
 
 <style lang="scss" scoped>
 .graphs {
-  width: 1rem;
-  height: 1rem;
-}
-
-#my-chart-id {
-  position: relative;
-  height: 10vh;
-  width: 80vw;
-  margin: 1rem;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 3rem;
+  margin-bottom: 5rem;
 }
 
 </style>

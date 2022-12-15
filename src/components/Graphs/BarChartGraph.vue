4400bfb99878014e854526cf1e9f405292203658
jurgen.telegrafi <jurgen.telegrafi@student.howest.be>
2022-12-15 13:53:51 +0100

    fetch from server into graph not working yet --> temp commit

--- src/components/Graphs/BarChartGraph.vue
+++ src/components/Graphs/BarChartGraph.vue
@@ -1,9 +1,60 @@
 <script>
 import {Bar} from 'vue-chartjs';
 import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js';
+import Gateway from "../../utils/events.js";
+
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
+export default {
+  name: 'BarChart',
+  components: {Bar},
+  computed: {
+    myStyles() {
+      return {
+        height: '50vh',
+        width: '25vw',
+      };
+    }
+  },
+  data: () => ({
+    loaded: false,
+    chartData: null,
+    chartOptions: {
+      responsive: false,
+    },
+  }),
+
+  async mounted() {
+    this.loaded = false
+    console.log(this.chartData);
+    try {
+      Gateway.onReady(() => {
+        Gateway.execute(Gateway.queries.GET_CRIMES_IN_AREA, {
+          userId: Gateway.clientId,
+          propertyId: "5",
+        }).then((data) => {
+          data.crimes.forEach(crime => {
+            this.chartData = {
+              labels: [crime.day + ","],
+              datasets: [{
+                label: 'Crimes',
+                backgroundColor: '#f87979',
+                data: [crime.count + ","]
+              }]
+            }
+          });
+        });
+        this.loaded = true
+      }, 1000);
+    } catch (err) {
+      console.error("Failed to get crimes", err);
+    }
+  }
+}
+
+
+/*
 export default {
   name: 'BarChartGraph',
   components: { Bar },
@@ -27,6 +78,18 @@ export default {
     };
   }
 };
+*/
+
+Gateway.onReady(() => {
+  Gateway.execute(Gateway.queries.GET_CRIMES_IN_AREA, {
+    userId: Gateway.clientId,
+    propertyId: "5",
+  }).then((data) => {
+    data.crimes.forEach(crime => {
+      console.log(crime);
+    });
+  });
+}, 1000);
 </script>
 <template>
   <Bar

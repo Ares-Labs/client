3d79a30437be8f35e7162a482b146e77e4745d08
raf.coulier <raf.coulier@student.howest.be>
2022-12-15 15:35:26 +0100

    map working

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,29 +1,67 @@
-<script setup>
-import Header from "@/components/Header/Header.vue";
+<script>
+import MapContainer from '../components/MapContainer.vue'
+//import Edit from './components/Edit'
+//import Inspect from './components/Inspect'
+
+export default {
+  name: 'MainPage',
+  components: {
+
+    MapContainer
+  },
+  data: () => ({
+    selected: undefined,
+    geojson: {
+      type: 'Feature',
+      properties: {
+        name: 'default object',
+        quality: 'top'
+      },
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [
+              -27.0703125,
+              43.58039085560784
+            ],
+            [
+              -28.125,
+              23.563987128451217
+            ],
+            [
+              -10.8984375,
+              32.84267363195431
+            ],
+            [
+              -27.0703125,
+              43.58039085560784
+            ]
+          ]
+        ]
+      }
+    }
+  })
+}
 </script>
+
 <template>
-  <Header></Header>
   <div id="app">
     <div class="cell cell-map">
-      Map
+      <MapContainer :geojson="geojson" v-on:select="selected = $event"></MapContainer>
     </div>
     <div class="cell cell-edit">
-      Edit
+      <Edit :geojson="geojson" v-on:change="geojson = $event"></Edit>
     </div>
     <div class="cell cell-inspect">
-      Inspect
+      <Inspect :feature="selected"></Inspect>
     </div>
   </div>
 </template>
 
 
-<script>
-export default {
-  name: "MapPage"
-}
-</script>
 
-<style scoped>
+<style>
 html, body {
   height: 100%;
   margin: 0;
@@ -49,7 +87,6 @@ html, body {
   grid-column: 1;
   grid-row-start: 1;
   grid-row-end: 3;
-  height: 25rem;
 }
 
 .cell-edit {

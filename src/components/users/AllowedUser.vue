1944e67d7470b0edb530b5d4899433713f6048a5
Firxstartxr <ryazanovprokhor@gmail.com>
2022-12-20 09:29:42 +0100

    refactored allowed users

--- src/components/users/AllowedUser.vue
+++ src/components/users/AllowedUser.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Gateway from "../../utils/events.js";
+import { ref } from "vue";
 
 const props = defineProps({
   name: String,
@@ -19,15 +20,17 @@ const propertyBeingManaged = window.location.pathname.split("/").pop();
 </script>
 
 <template>
-  <div class="allowedUser">
-    <div class="userinfo">
+  <div class="allowed-user">
+    <div class="allowed-user-info">
       <img src="/src/assets/media/profile.svg" alt="user-icon">
       <div>
-        <p> {{ name }} </p>
-        <p> ID: {{ identity }} </p>
-        <button @click="deleteUser(identity)">Delete user</button>
+        <p> {{name}} </p>
+        <p> ID: {{identity}} </p>
       </div>
     </div>
+    <a @click="deleteUser(identity)" class="btn">
+      <img src="/src/images/x.png" alt="delete">
+    </a>
   </div>
 </template>
 
@@ -36,12 +39,15 @@ const propertyBeingManaged = window.location.pathname.split("/").pop();
 @import "src/assets/css/mixins";
 @import "src/assets/css/app";
 
-.allowedUser {
+.allowed-user {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   border: 0.15rem solid $dark;
-  width: 15rem;
-  height: 5rem;
+  width: 20rem;
+  height: 3rem;
   padding: 0.3rem;
-  margin: 3rem;
+  margin-bottom: 1.5rem;
   border-radius: 1rem;
   background-color: #e8e8e8;
   box-shadow: $dark 0 0.5rem 0.3rem 0.01rem;
@@ -53,28 +59,28 @@ const propertyBeingManaged = window.location.pathname.split("/").pop();
       }
     }
   }
+
+  a {
+    margin-right: 1rem;
+  }
+
+  a:hover {
+    cursor: pointer;
+  }
 }
 
-.userinfo {
+.allowed-user-info {
   display: flex;
 }
 
 img {
   width: 2rem;
+  padding-left: 0.3rem;
   margin-right: 1rem;
-  padding-left: 0.1rem;
 }
 
-button {
-  @include button;
-}
-
-button:hover {
-  background-color: #852d26;
-}
-
-.hidden {
-  display: none;
+.btn {
+  @include img-hover;
 }
 
 </style>

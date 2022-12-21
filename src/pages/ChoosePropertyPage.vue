d60efe42b1a14bbe700636621cb4676d4c3e59b4
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-12-21 11:34:41 +0100

    testing 2

--- src/pages/ChoosePropertyPage.vue
+++ src/pages/ChoosePropertyPage.vue
@@ -11,8 +11,11 @@ Gateway.onReady(async () => {
   const response = await Gateway.execute(Gateway.queries.GET_USER_PROPERTIES, {
     userId: Gateway.clientId,
   });
-  properties.value = response.properties;
-  console.log(properties);
+  console.log(properties.value);
+  setTimeout(() => {
+    properties.value = response.properties;
+    console.log(properties.value);
+  }, 1000);
 });
 </script>
 
@@ -26,7 +29,7 @@ Gateway.onReady(async () => {
         </template>
         <template v-else-if="properties.length > 0">
           <property
-            v-for="property in properties"
+            v-for="property in properties ?? []"
             :key="property?.id"
             :name="property?.location"
             :tier="property?.tier"
